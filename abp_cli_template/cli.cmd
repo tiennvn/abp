ead4c2c04beaad6f7f86b66a86be3b1b5d5baad0
tienn <tienn.tmtco@gmail.com>
2023-06-20 14:15:41 +0700

    create local framework reference project

--- abp_cli_template/cli.cmd
+++ abp_cli_template/cli.cmd
@@ -1,1 +1,3 @@
 abp new Tien.New -t app -u mvc --tiered -o ./NewProject -dbms PostgreSQL --no-random-port
+
+abp new WiOn.Max -t app -u mvc --tiered -dbms PostgreSQL --no-random-port --local-framework-ref --abp-path ..\..\
