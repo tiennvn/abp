c92dd9afec68e036f7be9eba838416c7377f4a85
tienn <tienn.tmtco@gmail.com>
2023-06-21 14:08:05 +0700

    add new command

--- abp_cli_template/cli.cmd
+++ abp_cli_template/cli.cmd
@@ -1,3 +1,5 @@
-abp new Tien.New -t app -u mvc --tiered -o ./NewProject -dbms PostgreSQL --no-random-port
+abp new Tien.New -t app -u mvc --tiered -o ./Tien.New -dbms PostgreSQL --no-random-port
 
-abp new WiOn.Max -t app -u mvc --tiered -dbms PostgreSQL --no-random-port --local-framework-ref --abp-path ..\..\
+abp new WiOn.Max -t app -u mvc --tiered -dbms PostgreSQL --no-random-port --local-framework-ref --abp-path ..\..\..\..\
+
+abp new WiOn.Tizet -t app -u mvc --tiered -dbms PostgreSQL -o ./
