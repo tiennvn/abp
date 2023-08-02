fa2974878d321b634ee051cc81812cc7e8e79af8
tienn <tienn.tmtco@gmail.com>
2023-08-02 11:13:27 +0700

    add tye script

---
+++ project/WiOn.Tizet/Share/run-tye.ps1
@@ -0,0 +1,10 @@
+$currentFolder = $PSScriptRoot
+$devBuildFolder =  Join-Path $currentFolder "../"
+
+Write-Host "[INFO] START TYE SERVICES RUN WATCH" -ForegroundColor Yellow
+Set-Location $currentFolder
+$typePort = 30000
+Write-Host "[INFO] OPEN URL http://localhost:$typePort/ to view TYE DASHBOARD " -ForegroundColor Cyan
+<# Run all services #>
+tye run --watch --port $typePort --dashboard
+
